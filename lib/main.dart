4b73b71327d4087b78988a3a713a93d8ed209f8a
uas_adit <mmaditya611@gmail.com>
2025-08-07 17:13:53 +0700

    List product

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dit_ya/pages/alquran/list_alquran.dart';
 import 'package:dit_ya/pages/Post/list_posts_screen.dart';
 import 'package:dit_ya/pages/auth/login_screen.dart';
 import 'package:dit_ya/pages/home__screen.dart';
+import 'package:dit_ya/pages/menu_screen.dart';
 import 'package:dit_ya/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,6 @@ void main() async {
   final isLoggedIn = await authService.isLoggedIn();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: isLoggedIn ? HomeScreen() : LoginScreen(),
+    home: isLoggedIn ? MenuScreen() : LoginScreen(),
   ));
 }
---
+++ lib/pages/menu_screen.dart
@@ -0,0 +1,49 @@
+import 'package:dit_ya/pages/Post/list_posts_screen.dart';
+import 'package:dit_ya/pages/home__screen.dart';
+import 'package:dit_ya/pages/product/product_list.dart';
+import 'package:dit_ya/pages/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(), // Ganti dari Center(text: ...) ke HomeScreen()
+    ListPostScreen(),
+    ProductListScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: Text('Dashboard')),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Posts'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Produk',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
